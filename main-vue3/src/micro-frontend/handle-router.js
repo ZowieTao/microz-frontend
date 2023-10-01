d48b09c5f37df63dea11269fb5cf0573d26d4cc4
ZowieTao <zowietao@gmail.com>
2023-10-02 00:28:01 +0800

    feat: record previous and next route for unmount

--- main-vue3/src/micro-frontend/handle-router.js
+++ main-vue3/src/micro-frontend/handle-router.js
@@ -1,23 +1,80 @@
 import { getApps } from ".";
+import { importHTML } from "./import-html";
+import { getNextRoute, getPrevRoute } from "./rewrite-router";
 
 export const handleRouter = async () => {
   console.log("handleRouter", window.location.pathname);
   const apps = getApps();
 
+  // unmount previous sub app
+  const prevApp = apps.find((item) =>
+    getPrevRoute().startsWith(item.activeRule)
+  );
+  if (prevApp) {
+    await unmount(prevApp);
+  }
+
   // 2. match sub app
   const app = apps.find((item) => {
-    return window.location.pathname.startsWith(item.activeRule);
+    return getNextRoute().startsWith(item.activeRule);
   });
 
   if (!app) {
     return;
   }
 
+  // config env for sub app, effect resource load url
+  window.__POWERED_BY_QIANKUN__ = true;
+  window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__ = app.entry;
+
   // 3. load sub app
   // html css js
-  const html = await fetch(app.entry).then((res) => {
-    return res.text();
+  const container = document.querySelector(app.container);
+
+  // /**
+  //  * browser security do not execute script in innerHTML
+  //  * manually load script and execute use eval or new Function
+  //  */
+  // const html = await fetch(app.entry).then((res) => {
+  //   return res.text();
+  // });
+  // container.innerHTML = html;
+
+  const { template, getExternalScripts, execScripts } = await importHTML(
+    app.entry
+  );
+  container.appendChild(template);
+
+  getExternalScripts().then((scripts) => {
+    console.log(scripts);
   });
 
-  console.log("html", html);
+  const appExports = await execScripts();
+
+  console.log(appExports);
+
+  app.bootstrap = appExports.bootstrap;
+  app.mount = appExports.mount;
+  app.unmount = appExports.unmount;
+
+  await bootstrap(app);
+  await mount(app);
+  await unmount(app);
+
+  // 4. render
 };
+
+async function bootstrap(app) {
+  app.bootstrap && (await app.bootstrap());
+}
+
+async function mount(app) {
+  app.mount &&
+    (await app.mount({
+      container: document.querySelector(app.container),
+    }));
+}
+
+async function unmount(app) {
+  app.unmount && (await app.unmount());
+}
--- main-vue3/src/micro-frontend/rewrite-router.js
+++ main-vue3/src/micro-frontend/rewrite-router.js
@@ -1,5 +1,11 @@
 import { handleRouter } from "./handle-router";
 
+let prevRoute = "";
+let nextRoute = window.location.pathname;
+
+export const getPrevRoute = () => prevRoute;
+export const getNextRoute = () => nextRoute;
+
 export const rewriterRouter = () => {
   // 1.1 hash router:
   // window.addEventListener("hashchange", () => {
@@ -10,20 +16,29 @@ export const rewriterRouter = () => {
   // 1.2 history router
   window.addEventListener("popstate", () => {
     console.log("popstate handle");
+    // router was navigated while popstate trigger
+    prevRoute = nextRoute;
+    nextRoute = window.location.pathname;
     handleRouter();
   });
 
   const rawPushState = window.history.pushState;
   window.history.pushState = (...args) => {
+    // before navigate
+    prevRoute = window.location.pathname;
     rawPushState.apply(window.history, args);
-    console.log("pushState handle");
+    // after navigate
+    nextRoute = window.location.pathname;
     handleRouter();
   };
 
   const rawReplaceState = window.history.replaceState;
   window.history.replaceState = (...args) => {
+    // before navigate
+    prevRoute = window.location.pathname;
     rawReplaceState.apply(window.history, args);
-    console.log("replaceState handle");
+    // after navigate
+    nextRoute = window.location.pathname;
     handleRouter();
   };
 };
