4539e81553252ea67c552b210d44b2a8605d9abd
ZowieTao <zowietao@gmail.com>
2023-10-01 21:08:03 +0800

    chore: add vue2 sub app

---
+++ app-vue2/src/main.js
@@ -0,0 +1,35 @@
+import "../public-path";
+import Vue from "vue";
+import App from "./App.vue";
+
+Vue.config.productionTip = false;
+
+let instance = null;
+
+function render(props = {}) {
+  const { container } = props;
+
+  const shadowApp = container?.firstChild.shadowRoot.querySelector("#app");
+
+  instance = new Vue({
+    render: (h) => h(App),
+  }).$mount(shadowApp ? shadowApp : "#app");
+}
+
+// running not in main app
+if (!window.__POWERED_BY_QIANKUN__) {
+  render();
+}
+
+export async function bootstrap() {
+  console.log("[vue] vue app bootstraped");
+}
+export async function mount(props) {
+  console.log("[vue] props from main framework", props);
+  render(props);
+}
+export async function unmount() {
+  instance.$destroy();
+  instance.$el.innerHTML = "";
+  instance = null;
+}
---
+++ app-vue2/vue.config.js
@@ -0,0 +1,34 @@
+const packageName = require("./package.json").name;
+module.exports = {
+  publicPath: "/",
+  configureWebpack: {
+    output: {
+      // 必须打包出一个库文件
+      library: `${packageName}-[name]`,
+      // 库的格式必须是 umd (Universal Module Definition) Compatible with AMD, CommonJS or global variant
+      libraryTarget: "umd",
+    },
+  },
+  devServer: {
+    port: 2000,
+    headers: {
+      "Access-Control-Allow-Origin": "*", // allow CORS Request cause resource loaded to main app
+    },
+  },
+  css: {
+    // loaderOptions: {
+    //   css: {
+    //     // 注意：以下配置在 Vue CLI v4 与 v3 之间存在差异。
+    //     // Vue CLI v3 用户可参考 css-loader v1 文档
+    //     // https://github.com/webpack-contrib/css-loader/tree/v1.0.1
+    //     modules: {
+    //       localIdentName: '[name]-[hash]'
+    //     },
+    //     localsConvention: 'camelCaseOnly'
+    //   }
+    // },
+    // requireModuleExtension: false
+    extract: true,
+  },
+  productionSourceMap: false,
+};
